948db392b5feb113b7ca9d4c931c2eacbea75423
Jermol Jupiter <jermol@mtgfirst.com>
2018-12-26 18:22:28 -0500

    "changes made"

---
+++ engine/management/commands/add.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from subprocess import call
+
+def load_fixture(n):
+	call(['heroku', 'run', 'python', 'manage.py', 'loaddata', '{}.json'.format(str(n))])
+
+class Command(BaseCommand):
+    def handle(self, **options):
+    	current = int(input('Start:\n>'))
+    	stop = int(input('Stop:\n>'))
+    	while current <= stop:
+    		load_fixture(current)
+    		current += 1
+    		print current -= 1
+
+        
