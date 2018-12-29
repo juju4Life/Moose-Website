bff0b551f667b68d59716f2ea82f7ecd33e84566
Jermol Jupiter <jermol@mtgfirst.com>
2018-12-29 16:16:39 -0500

    "changes made"

--- source/urls.py
+++ source/urls.py
@@ -1,18 +1,4 @@
-"""webject URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -30,7 +16,8 @@ urlpatterns = [
     path(r'^__debug__/', include(debug_toolbar.urls)),
     path('sms/', include('sms.urls')),
     path('admin/', admin.site.urls),
-    path('', home_views.home, name='home'),
+    path('', home_views.home_base, name='base'),
+    path('home/', home_views.home, name='home'),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
