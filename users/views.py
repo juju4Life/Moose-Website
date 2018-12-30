8236cdae4bd50d2dc20bb9e94163f8da63dde58a
Jermol Jupiter <jermol@mtgfirst.com>
2018-12-29 23:56:35 -0500

    "changes made"

--- users/views.py
+++ users/views.py
@@ -6,26 +6,26 @@ from .forms import UserRegisterForm
 from customer.models import Customer
 
 def register(request):
-	if request.method == 'POST':
-		form = UserRegisterForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')			
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')            
 
-			messages.success(request, f'Account Created for {username}!')
-			return redirect('login')
-	else:
-		form = UserRegisterForm()
+            messages.success(request, f'Account Created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
 
-	return render(request, 'users/register.html', {'form':form})
+    return render(request, 'users/register.html', {'form':form})
 
 
 @login_required
 def profile(request):
-	#data = Customer.objects.get(email=)
-	username = None
+    #data = Customer.objects.get(email=)
+    username = None
     if request.user.is_authenticated():
         username = request.user.email
-	return render(request, 'users/profile.html', {'data': username})
+    return render(request, 'users/profile.html', {'data': username})
 
 
