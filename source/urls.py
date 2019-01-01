f74b346273cae63fbf6d3aacae212c22f1170b38
Jermol Jupiter <jermol@mtgfirst.com>
2019-01-01 08:51:58 -0500

    "changes made"

--- source/urls.py
+++ source/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password_reset', auth_views.PasswordResetView.as_view(template_name='users/registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
     path('password_reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', auth_views.PasswordResetConfirmView.as_view(success_url='password_reset_complete'), name='password_reset_confirm'),
     path('reset_done', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
