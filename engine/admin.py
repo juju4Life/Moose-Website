d5da0a27ee9311fd65e5aa96192470b1147e6373
Jermol Jupiter <jermol@mtgfirst.com>
2019-01-23 17:44:57 -0500

    "changes made"

--- engine/admin.py
+++ engine/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Orders, ForeignOrder, TcgCredentials, UpdatedInventory, CaseCards, StoreDatabase
+from .models import Product, Orders, ForeignOrder, TcgCredentials, UpdatedInventory, CaseCards, StoreDatabase, mtgDatabase
 from simple_history.admin import SimpleHistoryAdmin
 from customer.models import Preorder, Customer, PreordersReady, OrderRequest, ReleasedProducts
 from django.contrib.auth.models import User, Group
@@ -189,5 +189,6 @@ admin.site.register(Product, MasterAdmin)
 admin.site.register(ReleasedProducts, ReleasedProductsAdmin)
 admin.site.register(ForeignOrder, ForeignAdmin)
 admin.site.register(TcgCredentials)
+admin.site.register(mtgDatabase)
 admin.site.register(OrderRequest, OrderRequestAdmin)
 admin.site.unregister(Group)
