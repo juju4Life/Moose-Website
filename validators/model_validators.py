febff47fb385fb002708cda10f5e847912854167
Jermol Jupiter <jermol@mtgfirst.com>
2019-02-13 15:35:01 -0500

    "changes made"

--- validators/model_validators.py
+++ validators/model_validators.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 def validate_name(name):
-    if not name.isalpha():
+    if name.isalpha() is False:
         raise ValidationError(
             _('Name can only contain letters A-Z. Not numbers.'),
             params={'name': name},
