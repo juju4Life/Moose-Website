7d1b8a52e4b23848bc6caf5f828526446ecaa4f2
Jermol Jupiter <jermol@mtgfirst.com>
2019-03-04 11:35:43 -0500

    "changes made"

--- orders/management/commands/check_for_new_groups.py
+++ orders/management/commands/check_for_new_groups.py
@@ -1,10 +1,11 @@
 from django.core.management.base import BaseCommand
 from engine.tcgplayer_api import TcgPlayerApi
-from django.core.mail import send_mail
 from orders.models import GroupName
 from my_customs.decorators import report_error
+from engine.tcg_manifest import Manifest
 
 api = TcgPlayerApi()
+M = Manifest()
 
 
 class Command(BaseCommand):
@@ -13,16 +14,20 @@ class Command(BaseCommand):
         category_ids = [1, 2, 3, 31, 56, 16, 32, 27, 17, 29, 35, 14, 22]
         current_groups = GroupName.objects
 
-        for each in GroupName.objects.all():
-            if each.addded is False:
-                each.added = True
-                each.save()
-
-        '''for category in category_ids:
+        for category in category_ids:
             group_ids = api.get_group_ids(0, category)['results'][0:20]
             for group_id in group_ids:
-                if current_groups.filter():
-                    pass'''
+                group = str(group_id['groupId'])
+                if current_groups.filter(group_id=group).exists() is False:
+
+                    new_group = GroupName(
+                        group_id=group,
+                        group_name=group_id['name'],
+                        category=M.game(category),
+                        added=False,
+                    )
+                    new_group.save()
+                    print(group_id['name'] + ' added')
 
 
 
