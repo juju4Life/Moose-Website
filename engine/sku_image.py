fee7c1d260bd297f177ce26848e7d24b8f16a683
Jermol Jupiter <jermol@mtgfirst.com>
2019-03-23 13:58:38 -0400

    "changes made"

--- engine/sku_image.py
+++ engine/sku_image.py
@@ -1,15 +1,28 @@
 from django.core.management.base import BaseCommand
 from .models import MTG
 from .tcgplayer_api import TcgPlayerApi
+import requests
 
 tcg = TcgPlayerApi()
 
 
 class Command(BaseCommand):
     def handle(self, **options):
-        db = MTG.objects.filter(language='English')
+
+        def get_image(product_id):
+            url = f'https://api.scryfall.com/cards/tcgplayer/{product_id}'
+            r = requests.get(url)
+            return r.json()['image_uris']['normal']
+
+        db = MTG.objects.all()
 
         for each in db:
-            pass
+            sku = each.sku
+            product_id = tcg.card_info_by_sku(sku)['results'][0]['productId']
+            image = get_image(product_id)
+            each.product_id = product_id
+            each.image_url = image
+            each.save()
+
 
 
