a1c0dc49c96c03e61f45a40a0fa5767fd8030360
Jermol Jupiter <jermol@mtgfirst.com>
2019-03-30 14:59:47 -0400

    "changes made"

--- customer/models.py
+++ customer/models.py
@@ -6,6 +6,20 @@ from django.utils.translation import gettext_lazy as _
 from validators.model_validators import validate_name
 
 
+class ItemizedPreorder(models.Model):
+    name = models.CharField(max_length=255, default='')
+    quantity = models.IntegerField(default=0)
+    expansion = models.CharField(max_length=255, default='')
+    item_type = models.CharField(max_length=255, default='')
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    available = models.BooleanField(default=True)
+    image_url = models.CharField(max_length=255, default='')
+    product_id = models.CharField(max_length=50, default='')
+
+    def __str__(self):
+        return self.name
+
+
 class Preorder(models.Model):
     product = models.CharField(max_length=100, default='')
 
