83a53b941bed92b8c95c44a01ae659053ca7b1a3
Jermol Jupiter <jermol@mtgfirst.com>
2019-07-23 11:02:39 -0400

    "changes made"

--- engine/templates/gallery_scroll.js
+++ engine/templates/gallery_scroll.js
@@ -10,7 +10,7 @@
 		success: function(data){
 		    // Data set format = [[name, expansion, price, image], [name,...]]
             document.getElementById('gallery-image-1').setAttribute('src', data.cards[0][3]);
-            document.getElementById('image-expansion').innerHTML = `${data.cards[0][1])};
+            document.getElementById('image-expansion').innerHTML = `${data.cards[0][1]}:`;
             document.getElementById('gallery-image-2').setAttribute('src', data.cards[1][3]);
             document.getElementById('gallery-image-3').setAttribute('src', data.cards[2][3]);
 		},
