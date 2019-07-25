46a1125c60d428205da8b5edbb311fa3f272a906
Jermol Jupiter <jermol@mtgfirst.com>
2019-07-24 20:38:36 -0400

    "changes made"

--- engine/models.py
+++ engine/models.py
@@ -87,7 +87,6 @@ class Location(models.Model):
 
 
 class StoreDatabase(models.Model):
-
     name = models.CharField(max_length=255, default='', db_index=True)
     expansion = models.CharField(max_length=255, default='', db_index=True)
     price = models.DecimalField(max_digits=12, decimal_places=2, default=0, null=True, blank=True)
@@ -99,6 +98,22 @@ class StoreDatabase(models.Model):
     image = models.CharField(max_length=255, default='', blank=True, null=True)
     custom_percentage = models.IntegerField(null=True, default=0)
     language = models.CharField(max_length=255, default='', db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class DirectData(models.Model):
+    name = models.CharField(max_length=255, default='')
+    expansion = models.CharField(max_length=255, default='')
+    condition = models.CharField(max_length=255, default='')
+    language = models.CharField(max_length=255, default='English')
+    foil = models.BooleanField()
+    sku = models.CharField(max_length=255, default='')
+    product_id = models.CharField(max_length=255, default='')
+    consecutive_days_non_direct = models.IntegerField(default=0)
+    total_days_non_direct = models.IntegerField(default=0)
+    last_add = models.DateField()
 
     def __str__(self):
         return self.name
