59302b32a4cad245d42285fd14d6fffb1279563d
Jermol Jupiter <jermol@mtgfirst.com>
2019-08-06 21:07:45 -0400

    "changes made"

--- orders/management/commands/update_direct.py
+++ orders/management/commands/update_direct.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
                     if current_price > direct_low_price:
                         new_price = direct_low_price - .01
 
-                        if new_price < market_price * .7:
+                        if new_price < market_price * .8:
                             new_price = market_price
 
                         api.update_sku_price(sku_id=sku, price=new_price, _json=True)
