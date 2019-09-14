b48d7d23cadeae1a53b077a2c355601b84d0bb9f
Jermol Jupiter <jermol@mtgfirst.com>
2019-09-14 16:30:05 -0400

    "changes made"

--- orders/tasks.py
+++ orders/tasks.py
@@ -53,142 +53,141 @@ def update_moose_tcg():
                 if condition != 'Unopened':
                     current_price = card['currentPrice']
                     low = card['lowPrice']
-                    if current_price != low:
-                        sku = card['skuId']
-                        product_id = card['productId']
-                        name = card['productName']
-                        expansion = card['groupName']
-                        market = card['marketPrice']
-                        language = card['languageName']
+                    sku = card['skuId']
+                    product_id = card['productId']
+                    name = card['productName']
+                    expansion = card['groupName']
+                    market = card['marketPrice']
+                    language = card['languageName']
 
-                        '''    
-                        If the card is not English it will be priced at the low price minus one cent.
+                    '''    
+                    If the card is not English it will be priced at the low price minus one cent.
 
-                        For each card in the MooseLoot inventory we will make a request to the tcgplayer page containing all seller data for a given 
-                        product. 
-                        We request and scan pages (10 results per page) until we find 2 listings with sellers that have 10,000 sales or more. We break the while loop 
-                        once we have found those two listings and move on to the next card. In the case where only one or zero listings are found, 
-                        we break the loop and use one price to match against or default to the market price.      
-                        '''
+                    For each card in the MooseLoot inventory we will make a request to the tcgplayer page containing all seller data for a given 
+                    product. 
+                    We request and scan pages (10 results per page) until we find 2 listings with sellers that have 10,000 sales or more. We break the while loop 
+                    once we have found those two listings and move on to the next card. In the case where only one or zero listings are found, 
+                    we break the loop and use one price to match against or default to the market price.      
+                    '''
 
-                        if language != 'English' and printing != 'Foil':
-                            # catch instances where there is no low price
-                            try:
-                                updated_price = low - .01
-                            except TypeError:
-                                updated_price = None
+                    if language != 'English' and printing != 'Foil':
+                        # catch instances where there is no low price
+                        try:
+                            updated_price = low - .01
+                        except TypeError:
+                            updated_price = None
 
-                            if updated_price is not None:
-                                api.update_sku_price(sku_id=sku, price=updated_price, _json=True)
+                        if updated_price is not None:
+                            api.update_sku_price(sku_id=sku, price=updated_price, _json=True)
 
-                        elif language == 'English':
+                    elif language == 'English':
 
-                            next_page = True
-                            page = 1
-                            seller_data_list = []
+                        next_page = True
+                        page = 1
+                        seller_data_list = []
 
-                            while next_page is True:
+                        while next_page is True:
 
-                                request_path = url(product_id=product_id, condition=condition, foil=printing, page=page)
+                            request_path = url(product_id=product_id, condition=condition, foil=printing, page=page)
 
-                                data, page_source = request_pages_data(
-                                    url=request_path,
-                                    tag='div',
-                                    attribute='class',
-                                    attribute_value='product-listing ',
-                                )
-
-                                # Check if there are products in the request. If not that indicates no more listings and thus we break the loop
-                                if not data:
-                                    break
-
-                                # loop over each item on the page and get Seller Info
-                                for d in data:
-                                    seller_condition = d.find('div', {'class': 'product-listing__condition'}).text.strip()
-                                    seller_name = d.find('a', {'class': 'seller__name'}).text.strip()
-
-                                    if seller_name != 'MTGFirst' and seller_name != 'Moose Loot' and condition == seller_condition:
-
-                                        price, total_price, seller_total_sales = get_product_seller_info(d)
-
-                                        price_dict = {
-                                            'price': total_price,
-                                            'gold': True if seller_total_sales >= 10000 else False,
-                                        }
-
-                                        seller_data_list.append(price_dict)
-                                        if len(seller_data_list) == 5:
-                                            next_page = False
-                                            break
-
-                                page += 1
-
-                            '''
-                            We will check the number of other seller listings.
-                            If there were zero listings found we simply make the updated price the market price.
-
-                            If just one listing is found, we run the price algorithm which will just add shipping if default and price .01c less.
-
-                            If there are 2 10,000+ listings, algorithm will compare and take the best/cheapest listings price
-                            '''
-
-                            updated_price = moose_price_algorithm(seller_data=seller_data_list, )
-
-                            '''
-                             new = moose_inventory.create(
-                                name=card_data['card_name'],
-                                expansion=card_data['card_set'],
-                                condition=card_data['card_condition'],
-                                printing=printing,
-                                seller_1_name=card_data['seller_1_name'],
-                                seller_1_total_sales=card_data['seller_1_total_sales'],
-                                seller_1_total_price=card_data['seller_1_total_price'],
-                                seller_2_name=card_data['seller_2_name'],
-                                seller_2_total_sales=card_data['seller_2_total_sales'],
-                                seller_2_total_price=card_data['seller_2_total_price'],
-                                updated_price=card_data['updated_price'],
-
+                            data, page_source = request_pages_data(
+                                url=request_path,
+                                tag='div',
+                                attribute='class',
+                                attribute_value='product-listing ',
                             )
 
-                            new.save()
-                            '''
+                            # Check if there are products in the request. If not that indicates no more listings and thus we break the loop
+                            if not data:
+                                break
 
-                            if updated_price is not None and round(updated_price, 2) != current_price:
-                                # print(index)
-                                api.update_sku_price(sku_id=sku, price=updated_price, _json=True)
-                                metrics, created = MooseAutopriceMetrics.objects.get_or_create(sku=sku)
-                                metrics.name = name
-                                metrics.expansion = expansion
-                                metrics.condition = condition
-                                metrics.printing = printing
-                                metrics.language = language
-                                metrics.old_price = current_price
-                                metrics.updated_price = updated_price
-                                count = 0
+                            # loop over each item on the page and get Seller Info
+                            for d in data:
+                                seller_condition = d.find('div', {'class': 'product-listing__condition'}).text.strip()
+                                seller_name = d.find('a', {'class': 'seller__name'}).text.strip()
 
-                                while count <= len(seller_data_list):
-                                    if count == 0:
-                                        metrics.price_1 = seller_data_list[count]['price']
-                                        metrics.price_1_gold = seller_data_list[count]['gold']
-                                    elif count == 1:
-                                        metrics.price_2 = seller_data_list[count]['price']
-                                        metrics.price_2_gold = seller_data_list[count]['gold']
-                                    elif count == 2:
-                                        metrics.price_3 = seller_data_list[count]['price']
-                                        metrics.price_3_gold = seller_data_list[count]['gold']
-                                    elif count == 3:
-                                        metrics.price_4 = seller_data_list[count]['price']
-                                        metrics.price_4_gold = seller_data_list[count]['gold']
-                                    elif count == 4:
-                                        metrics.price_5 = seller_data_list[count]['price']
-                                        metrics.price_5_gold = seller_data_list[count]['gold']
-                                    count += 1
+                                if seller_name != 'MTGFirst' and seller_name != 'Moose Loot' and condition == seller_condition:
 
-                                metrics.save()
+                                    price, total_price, seller_total_sales = get_product_seller_info(d)
 
-                                if index < 100:
-                                    print(name, expansion, condition, printing)
-                                    print(f"Current: {current_price}, Market: {market}, Low: {low}, Updated: {updated_price}")
+                                    price_dict = {
+                                        'price': total_price,
+                                        'gold': True if seller_total_sales >= 10000 else False,
+                                    }
+
+                                    seller_data_list.append(price_dict)
+                                    if len(seller_data_list) == 5:
+                                        next_page = False
+                                        break
+
+                            page += 1
+
+                        '''
+                        We will check the number of other seller listings.
+                        If there were zero listings found we simply make the updated price the market price.
+
+                        If just one listing is found, we run the price algorithm which will just add shipping if default and price .01c less.
+
+                        If there are 2 10,000+ listings, algorithm will compare and take the best/cheapest listings price
+                        '''
+
+                        updated_price = moose_price_algorithm(seller_data=seller_data_list, )
+
+                        '''
+                         new = moose_inventory.create(
+                            name=card_data['card_name'],
+                            expansion=card_data['card_set'],
+                            condition=card_data['card_condition'],
+                            printing=printing,
+                            seller_1_name=card_data['seller_1_name'],
+                            seller_1_total_sales=card_data['seller_1_total_sales'],
+                            seller_1_total_price=card_data['seller_1_total_price'],
+                            seller_2_name=card_data['seller_2_name'],
+                            seller_2_total_sales=card_data['seller_2_total_sales'],
+                            seller_2_total_price=card_data['seller_2_total_price'],
+                            updated_price=card_data['updated_price'],
+
+                        )
+
+                        new.save()
+                        '''
+
+                        if updated_price is not None and round(updated_price, 2) != current_price:
+                            # print(index)
+                            api.update_sku_price(sku_id=sku, price=updated_price, _json=True)
+                            metrics, created = MooseAutopriceMetrics.objects.get_or_create(sku=sku)
+                            metrics.name = name
+                            metrics.expansion = expansion
+                            metrics.condition = condition
+                            metrics.printing = printing
+                            metrics.language = language
+                            metrics.old_price = current_price
+                            metrics.updated_price = updated_price
+                            count = 0
+
+                            while count < len(seller_data_list):
+                                if count == 0:
+                                    metrics.price_1 = seller_data_list[count]['price']
+                                    metrics.price_1_gold = seller_data_list[count]['gold']
+                                elif count == 1:
+                                    metrics.price_2 = seller_data_list[count]['price']
+                                    metrics.price_2_gold = seller_data_list[count]['gold']
+                                elif count == 2:
+                                    metrics.price_3 = seller_data_list[count]['price']
+                                    metrics.price_3_gold = seller_data_list[count]['gold']
+                                elif count == 3:
+                                    metrics.price_4 = seller_data_list[count]['price']
+                                    metrics.price_4_gold = seller_data_list[count]['gold']
+                                elif count == 4:
+                                    metrics.price_5 = seller_data_list[count]['price']
+                                    metrics.price_5_gold = seller_data_list[count]['gold']
+                                count += 1
+
+                            metrics.save()
+
+                            if index < 100:
+                                print(name, expansion, condition, printing)
+                                print(f"Current: {current_price}, Market: {market}, Low: {low}, Updated: {updated_price}")
 
             except Exception as e:
                 print(e)
