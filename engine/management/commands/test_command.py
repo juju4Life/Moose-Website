1c8dc9af647d41dc2ecdfd6e2e90f89cac235cee
Jermol Jupiter <jermol@mtgfirst.com>
2019-11-23 21:30:00 -0500

    "changes made"

--- engine/management/commands/test_command.py
+++ engine/management/commands/test_command.py
@@ -1,12 +1,14 @@
 from django.core.management.base import BaseCommand
 from buylist.ck_buylist import ck_buylist, get_page_count
 from buylist.scg_buylist import get_scg_buylist
+from engine.get_group_prices import get_tcg_prices
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         ck_buylist(get_page_count())
         get_scg_buylist()
+        get_tcg_prices()
 
 
 
