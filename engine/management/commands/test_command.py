8bebe3aecaac1bf2b277f813838d4cad7fc28dee
Jermol Jupiter <jermol@mtgfirst.com>
2019-12-09 12:57:21 -0500

    "changes made"

--- engine/management/commands/test_command.py
+++ engine/management/commands/test_command.py
@@ -13,20 +13,40 @@ from engine.get_group_prices import get_tcg_prices
 class Command(BaseCommand):
     @report_error
     def handle(self, *args, **options):
-        three_start = time()
-        get_tcg_prices()
-        three_stop = time()
-        three_elapsed = (three_stop - three_start) / 3600
 
         start = time()
         ck_buylist(get_page_count())
         end = time()
         elapsed = (end - start) / 3600
 
+        send_mail(
+            subject='CK Buylist for hub',
+            message=f'Done in {elapsed} hours',
+            from_email='TCG FIRST',
+            recipient_list=['jermol.jupiter@gmail.com', ]
+        )
+
         two_start = time()
         get_scg_buylist()
         two_stop = time()
         two_elapsed = (two_stop - two_start) / 3600
+        send_mail(
+            subject='SCG Buylist for hub',
+            message=f'Done in {two_elapsed} hours',
+            from_email='TCG FIRST',
+            recipient_list=['jermol.jupiter@gmail.com', ]
+        )
+
+        three_start = time()
+        get_tcg_prices()
+        three_stop = time()
+        three_elapsed = (three_stop - three_start) / 3600
+        send_mail(
+            subject='TCG PRices for Hub',
+            message=f'Done in {three_elapsed} hours',
+            from_email='TCG FIRST',
+            recipient_list=['jermol.jupiter@gmail.com', ]
+        )
 
         four_start = time()
         add_buylist_data()
