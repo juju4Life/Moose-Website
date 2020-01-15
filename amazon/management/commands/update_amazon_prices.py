5299346e7bf59edbcd4292cf674e89f2d56a3840
Jermol Jupiter <jermol@mtgfirst.com>
2020-01-15 17:40:04 -0500

    "changes made"

--- amazon/management/commands/update_amazon_prices.py
+++ amazon/management/commands/update_amazon_prices.py
@@ -151,6 +151,8 @@ class Command(BaseCommand):
 
                                                     elif competitive_price > max_price:
                                                         competitive_price = max_price
+                                                        if competitive_price > old_price * 1.5:
+                                                            competitive_price = old_price * 1.5
                                                     data.price_metrics = price_list
                                                     data.save()
 
