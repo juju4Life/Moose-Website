99d6544e13da38561d22e3044073ddce0f31aea4
Jermol Jupiter <jermol@mtgfirst.com>
2020-02-24 16:55:49 -0500

    "changes made"

--- layout/admin.py
+++ layout/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import HomePageImage
+'''
+from .models import HomePageLayout
 
 
-@admin.register(HomePageImage)
+@admin.register(HomePageLayout)
 class HomePageAdmin(admin.ModelAdmin):
     pass
+    
+    
+
+'''
--- layout/models.py
+++ layout/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 
-class HomePageImage(models.Model):
+class HomePageLayout(models.Model):
     name = models.CharField(max_length=255, default='')
     image = CloudinaryField('image')
     text = models.TextField(blank=True)
