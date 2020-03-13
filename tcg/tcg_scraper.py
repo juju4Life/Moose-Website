21608cdc98002e3409387c931e3e86fcb41b1aad
Jermol Jupiter <jermol@mtgfirst.com>
2020-03-13 18:56:41 -0400

    "changes made"

--- tcg/tcg_scraper.py
+++ tcg/tcg_scraper.py
@@ -9,15 +9,15 @@ from selenium.webdriver.chrome.options import Options
 
 class TcgScraper:
     def __init__(self):
-        self.GOOGLE_CHROME_BIN = config('GOOGLE_CHROME_BIN')
+        # self.GOOGLE_CHROME_BIN = config('GOOGLE_CHROME_BIN')
         self.GOOGLE_CHROME_SHIM = config('GOOGLE_CHROME_SHIM')
         self.chrome_options = Options()
         self.chrome_options.add_argument("--disable-dev-shm-usage")
         self.chrome_options.add_argument('--disable-gpu')
         self.chrome_options.add_argument('--no-sandbox')
         self.chrome_options.add_argument("--headless")
-        self.chrome_options.binary_location = self.GOOGLE_CHROME_BIN
-        self.driver = webdriver.Chrome(executable_path=self.GOOGLE_CHROME_SHIM, options=self.chrome_options)
+        self.chrome_options.binary_location = self.GOOGLE_CHROME_SHIM
+        self.driver = webdriver.Chrome(options=self.chrome_options)
 
     def quit_driver(self):
         self.driver.quit()
