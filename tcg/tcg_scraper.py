73277a748290dc059dbcbe68a8a2b529702e83d3
Jermol Jupiter <jermol@mtgfirst.com>
2020-07-13 13:05:54 -0400

    "changes made"

--- tcg/tcg_scraper.py
+++ tcg/tcg_scraper.py
@@ -26,7 +26,6 @@ class TcgScraper:
         if self.GOOGLE_CHROME_SHIM != 'Local':
             self.chrome_options.binary_location = self.GOOGLE_CHROME_SHIM
             self.driver = webdriver.Chrome(options=self.chrome_options)
-            pass
         else:
             self.driver = webdriver.Chrome(executable_path='chromeDriver/chromedriver', options=self.chrome_options)
 
