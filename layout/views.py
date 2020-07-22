0a559c862d0db948a8be9f4be69cafd7b4cd5028
Jermol Jupiter <jermol@mtgfirst.com>
2020-07-22 13:59:09 -0400

    "changes made"

--- layout/views.py
+++ layout/views.py
@@ -15,9 +15,9 @@ def daily_mtg_hook(request):
     print(request.META)
     print(request.headers)
     print(request.read())
-    print(request.read_line())
     challenge = request.GET.get("hub.challenge")
     topic = request.GET.get("hub.topic")
+    print(topic)
 
     return HttpResponse(challenge)
 
