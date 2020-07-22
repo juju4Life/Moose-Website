039c0c1547d73776a03e71f30512e9357208bdc8
Jermol Jupiter <jermol@mtgfirst.com>
2020-07-22 12:19:34 -0400

    "changes made"

--- layout/views.py
+++ layout/views.py
@@ -2,21 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-import requests
 
 
 @csrf_exempt
 @require_http_methods(["GET", "POST", ])
 def daily_mtg_hook(request):
-    template = "layout/templates/daily_mtg.html"
-    context = dict()
-
+    print(request.body)
     print(request.GET)
     print(request.POST)
     challenge = request.GET.get("hub.challenge")
     topic = request.GET.get("hub.topic")
-    print(topic)
-    print(challenge)
 
     return HttpResponse(challenge)
 
